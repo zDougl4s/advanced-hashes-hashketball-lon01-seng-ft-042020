6fa51d1f85e7ff56fd8390a0f9cf26453b815f60
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-15 19:00:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,7 +132,7 @@ def num_points_scored (player)
   data = game_hash
    data.each do |home_away,stats|
      stats.each do |stats_list,result_stats|
-       
+       stats_list
      binding.pry
 
    end
