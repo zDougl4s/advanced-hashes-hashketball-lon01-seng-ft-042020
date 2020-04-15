7362d5fa12c1a13794ee56b5e377c9e4dcfbffc0
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-15 20:15:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
   {
     home: {
       team_name: "Brooklyn Nets",
-      colors: ["Black]", "White"],
+      colors: ["Black", "White"],
       players: [
         {
           player_name: "Alan Anderson",
